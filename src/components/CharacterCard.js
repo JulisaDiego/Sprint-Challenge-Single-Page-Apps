b5e7b99f7bec61fbfe3aeceff96e75bcf3e3eb61
JulisaDiego <julisadiego@yahoo.com>
2020-01-24 20:21:43 -0500

    mvp, no styles

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,29 @@
 import React from "react";
+import {Link} from 'react-router-dom';
+import CharacterList from "./CharacterList";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
-}
+
+
+
+export default function CharacterCard(props, image) {
+  return(
+
+      <div className="wrapper">
+        <div className="char-card">
+            <h2>{props.name}</h2>
+    
+
+        <div className="species">
+          <p>Species: {props.species}</p>
+        </div>
+
+        <div className="status">
+          <p>Status: {props.status}</p>
+        </div>
+
+      </div>
+
+    </div>
+  );
+
+  }
